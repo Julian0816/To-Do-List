e88a46f75ebabd498af32d4c5b012790b22fab0c
Julian0816 <julian.martinez.cs@gmail.com>
2023-03-29 23:36:02 +0000

    todos maping and rendering, validation and counter completed

--- todos-v2/src/components/Todos.js
+++ todos-v2/src/components/Todos.js
@@ -4,26 +4,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './Todos.css'
 
 const Todos = () => {
+    const [toDos, setToDos] = useState([]);
+    const [inputValue, setInputValue] = useState('');
 
-    const [inputValue, setInputValue ] = useState('');
-    
+    const newTodos = () => {
+        if (inputValue === '') {
+            return
+        } else {
+        setToDos([...toDos, inputValue.trim()]);
+        console.log(toDos)
+        }
+    }
+
     const validateInput = () => {
-      if(inputValue === "") alert("You cannot save an empty todo");
-      else alert("Todo saved");
+        if (inputValue === "") alert("You cannot save an empty todo");
+        else alert("Todo saved");
     };
+
+    function handleKeyDown(event) {
+        if (event.key === 'Enter') {
+            console.log('Enter key pressed');
+        }
+    }
 
     return (
         <div className="mainContainer">
             <div>
                 <h1 className="header">todos</h1>
-            </div>        
+            </div>
+            <input className="input" type="text" onChange={e => setInputValue(e.target.value)} value={inputValue} />
             <ul className="u-li">
-                {<li><input className="input" type="text" onChange={e => setInputValue(e.target.value)} value={inputValue} /></li>}
-            <div className="todos-left"><p className="todos-left-text">X todos left</p></div>
-            <div className="extra-style-1"></div>
-            <div className="extra-style-2"></div>
+                {toDos.map((todo, index) => (<li key={index} className="input"><p>{todo === '' ? stop : todo}</p></li>))}
+                <div className="todos-left"><p className="todos-left-text">{toDos.length} todos left</p></div>
+                <div className="extra-style-1"></div>
+                <div className="extra-style-2"></div>
             </ul>
-            <Button onClick={validateInput} variant="light">Save todo</Button>
+            <Button onClick={newTodos} onClickCapture={validateInput} variant="light">Save todo</Button>
+            {/* <button className="saveTodos" onClick={newTodos}>Save</button> */}
         </div>
     )
 }
