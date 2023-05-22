dfa3d276ca193ce57b69d7e0c01f6f1d65f61802
Julian Martinez <julian.martinez.cs@gmail.com>
2023-05-22 11:23:26 +0000

    Exercise fixed, now within the template

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [toDos, setToDos] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    const [isHovered, setIsHovered] = useState(false);
+
+    const newTodos = () => {
+        if (inputValue === '') {
+            return
+        } else {
+            setToDos([...toDos, inputValue.trim()]);
+            console.log(toDos)
+        }
+    }
+
+    const validateInput = () => {
+        if (inputValue === "") alert("No tasks, add a task");
+    };
+
+    const deleteTodo = (index) => {
+        const newTodos = [...toDos];
+        newTodos.splice(index, 1);
+        setToDos(newTodos);
+      };
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div >
+            <div>
+                <h1 className="header">todos</h1>
+            </div>
+            <input className="input" type="text" onChange={e => setInputValue(e.target.value)} value={inputValue} />
+            <ul className="u-li">
+                {toDos.map((todo, index) => (<div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}><li key={index} className="input"><p>{todo}{isHovered && <Button onClick={deleteTodo} variant="light">x</Button>}</p></li></div>))}
+                <div className="todos-left"><p className="todos-left-text">{toDos.length} items left</p></div>
+                <div className="extra-style-1"></div>
+                <div className="extra-style-2"></div>
+            </ul>
+            <Button onClick={newTodos} onClickCapture={validateInput} variant="light">Save</Button> 
+        </div>
 		</div>
 	);
 };
